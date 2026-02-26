af625a60d50e6731ba0ea7f679e69e3f65fc786d
wenithqp-cloud <wenithqp@gmail.com>
2026-02-26 10:30:04 -0800

    Delete frontend directory

--- frontend/sw.js
+++
@@ -1,6 +0,0 @@
-self.addEventListener("install", e=>{
-  e.waitUntil(caches.open("music-app").then(cache=>cache.addAll([
-    "index.html","register.html","app.html","settings.html",
-    "style.css","theme.js","app.js"
-  ])));
-});
